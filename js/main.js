d39ee992e5f66678fd62fa8188e282df60a2a301
camouflaged-penguin <82024665+camouflaged-penguin@users.noreply.github.com>
2021-04-29 10:52:15 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,48 +4,24 @@ var endgame;
 
 
 
-window.onload = function() {
-    var isMobile = navigator.userAgent.indexOf("Mobile");
-    if (isMobile == -1) {
-        isMobile = navigator.userAgent.indexOf("Tablet");
-    }
-    //
-    //
-    if (isMobile == -1) {
-        var config = {
-            type: Phaser.AUTO,
-            width: 1920,
-            height: 1080,
-            parent: 'phaser-game',
-            physics: {
-                default: 'arcade',
-                arcade: {
-                gravity: { y: 300 },
-                debug: false
-              }
-            },
-            scene: [SceneMain, SceneOver]
-        };
-    } else {
-        var config = {
+window.onload=function()
+{
+  var config = {
         type: Phaser.AUTO,
-        width: window.innerWidth,
-        height: window.innerHeight,
-        parent: 'phaser-game',
+        width: 1920,
+        height: 1080,
         physics: {
             default: 'arcade',
             arcade: {
-            gravity: { y: 300 },
-            debug: false
-          }
+                gravity: { y: 300 },
+                debug: false
+            }
         },
+        parent: 'phaser-game',
         scene: [SceneMain, SceneOver]
     };
-    }
-
-    game = new Phaser.Game(config);
-  }
-
+     game = new Phaser.Game(config);
+}
 
 
 
